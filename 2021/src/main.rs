907d8af6659c00fa8feabcf0612c0f6294cd91ca
Arturas Slajus <x11@arturaz.net>
2022-02-01 21:36:36 +0200

    2021: day 6 parts 1 & 2

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -10,9 +10,10 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
-    day5::part2();
+    day6::part2();
 }
 
 pub fn read_lines(path: &str) -> Map<Lines<BufReader<File>>, fn(std::io::Result<String>) -> String> {
